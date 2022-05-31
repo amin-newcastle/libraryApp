c5a1f48063e2f8955c8257d733e22a6f7f28405c
Amin <muhammad-karim@hotmail.com>
2022-05-31 10:43:18 +0100

    refactory book form

--- src/app/forms/book-form.component.ts
+++ src/app/forms/book-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BookService} from "../services/book/book.service";
+import {Book} from "../models/book.interface";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-forms',
@@ -7,13 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  public book!: Book;
+
+
+  constructor(private bookService: BookService, private router: Router) { }
 
   ngOnInit(): void {
+    this.book = {
+      ISBN: 9,
+      title: "",
+      author: "",
+      synopsis: ""
+    }
   }
 
-  createBook() {
+  async submit(){
+    await this.createBook(this.book);
+    // console.log(this.book);
+  }
 
+  async createBook(book: Book) {
+    const response: any = await this.bookService.addBook(book);
+    if (response?.success) {
+      console.log('success');
+      const isbn = response.isbn;
+      await this.router.navigate(['/book', isbn]);
+    }
   }
 
 }
